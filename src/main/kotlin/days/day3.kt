e6fefe9155d02cc6b6f32ed692d39abfb2a9dd8f
Eugen Sawitzki <e.sawitzki@reply.de>
2023-08-28 10:54:48 +0200

    day 3 and 4 for 2022

--- src/main/kotlin/days/day3.kt
+++ src/main/kotlin/days/day3.kt
@@ -1,20 +1,46 @@
 package days
 
-import java.lang.Exception
+import api.fetchData
+import kotlin.math.floor
 
 fun day3(part: Int) {
+    val data = fetchData(3)
+    val backpacks = data.removeSuffix("\n").split("\n")
+
     when (part) {
-        1 -> part1()
-        2 -> part2()
+        1 -> part1(backpacks)
+        2 -> part2(backpacks)
         else -> throw Exception("Unknown part $part")
     }
 }
 
-private fun part1() {
-    println("day 3; part 1")
+private fun part1(backpacks: List<String>) {
+    val compartments = backpacks.map {
+        val middleIndex = floor((it.length / 2).toDouble()).toInt()
+        Pair(it.substring(0, middleIndex), it.substring(middleIndex))
+    }
+    val result = compartments.sumOf {
+        val first = it.first.splitWithoutEmptyStrings().toSet()
+        val second = it.second.splitWithoutEmptyStrings().toSet()
+        val code = first.intersect(second).first().first().code
+
+        getPriorityForCharCode(code)
+    }
+
+    println(result)
 }
 
-private fun part2() {
-    println("day 3; part 2")
+private fun part2(backpacks: List<String>) {
+    val result = backpacks.chunked(3).sumOf {
+        val code = it[0].splitWithoutEmptyStrings().intersect(it[1].splitWithoutEmptyStrings().toSet())
+            .intersect(it[2].splitWithoutEmptyStrings().toSet()).first().first().code
+
+        getPriorityForCharCode(code)
+    }
+
+    println(result)
 }
 
+private fun String.splitWithoutEmptyStrings() = this.split("").filter { it != "" }
+
+private fun getPriorityForCharCode(charCode: Int) = if (charCode <= 90) charCode - 38 else charCode - 96
--- src/main/kotlin/days/day4.kt
+++ src/main/kotlin/days/day4.kt
@@ -1,19 +1,48 @@
 package days
 
+import api.fetchData
 import java.lang.Exception
 
+data class Range(val start: Int, val end: Int)
+
 fun day4(part: Int) {
+    val data = fetchData(4)
+    val rangePairs: List<Pair<Range, Range>> = data.removeSuffix("\n").split("\n")
+        .map { row ->
+            val entries = row.split(",")
+            val firstRange = entries[0].split("-")
+            val secondRange = entries[1].split("-")
+            Pair(
+                Range(firstRange[0].toInt(), firstRange[1].toInt()),
+                Range(secondRange[0].toInt(), secondRange[1].toInt())
+            )
+        }
+
     when (part) {
-        1 -> part1()
-        2 -> part2()
+        1 -> part1(rangePairs)
+        2 -> part2(rangePairs)
         else -> throw Exception("Unknown part $part")
     }
 }
 
-private fun part1() {
-    println("day 4; part 1")
+private fun part1(rangePairs: List<Pair<Range, Range>>) {
+    val result = rangePairs.count {
+        (it.first.start <= it.second.start && it.first.end >= it.second.end) || (it.second.start <= it.first.start && it.second.end >= it.first.end)
+    }
+    println(result)
 }
 
-private fun part2() {
-    println("day 4; part 2")
+private fun part2(rangePairs: List<Pair<Range, Range>>) {
+    val result = rangePairs.count {
+        (it.first.start <= it.second.start && it.first.end >= it.second.start)
+                || (it.first.end >= it.second.end && it.first.start <= it.second.end)
+                || (it.second.start <= it.first.start && it.second.end >= it.first.start)
+                || (it.second.end >= it.first.end && it.second.start <= it.first.end)
+    }
+    println(result)
+}
+
+private fun rangeStringToRangeList(rangeString: String): IntRange {
+    val splitRange = rangeString.split("-")
+    return splitRange[0].toInt()..splitRange[1].toInt()
 }
